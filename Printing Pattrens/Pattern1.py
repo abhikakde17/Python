73ad6c9e239f49cb504759a8b068c84cecb0a98f
Kakde Dattprasad <abhikakde1704@gmail.com>
2025-09-09 23:46:00 +0530

    Added Printing Patterns.

---
+++ Printing Pattrens/Pattern1.py
@@ -0,0 +1,15 @@
+'''
+Q1. Print the Pattern.
+
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+
+'''
+
+for i in range (1,6):
+    for j in range (1,6):
+        print("*", end=" ")
+    print()
